4162afc6a05670edb9bee274ed186ec0196d0381
Kumanan Murugesan <kumanan12@gmail.com>
2024-10-30 14:08:02 -0400

    first commit.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { add } from './add.js';
+import { sub } from './sub.js';
+import { multiply } from './multiply.js';
+import { divide } from './divide.js';
+let addResult = add(1, 2);
+let subResult = sub(9, 2);
+let mulResult = multiply(71, 2);
+let divResult = divide(8, 2);
+console.log(addResult, subResult, mulResult, divResult);
